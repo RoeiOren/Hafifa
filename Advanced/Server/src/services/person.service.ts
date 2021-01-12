da0fb50a8d6ce3bfce1bae6ed9c722300f189e69
roei oren <roeioren10@gmail.com>
2021-01-12 10:58:20 +0200

    add service

--- Advanced/Server/src/services/person.service.ts
+++ Advanced/Server/src/services/person.service.ts
@@ -2,6 +2,26 @@ const getAll = () => {
 
 }
 
+const getById = (id) => {
+
+}
+
+const getByName = (name) => {
+
+}
+
+const addPerson = (newPerson) => {
+
+}
+
+const updatePerson = (updatedPerson) => {
+
+}
+
+const deletePerson = (personToDelete) => {
+
+}
+
 
 module.exports = {
     getAll,
