2a2ce979d05a5bf7e1a5d17951611299a20a121d
roei oren <roeioren10@gmail.com>
2021-01-12 11:11:19 +0200

    added router

---
+++ Advanced/Server/src/routers/group.router.ts
@@ -0,0 +1,19 @@
+import express from "express";
+const router = express.Router();
+
+// get all groups
+router.get('/', (req, res) => {
+
+})
+
+// get by id
+router.get('/:id', (req, res) => {
+
+})
+
+// get by group name
+router.get('/:name', (req, res) => {
+
+})
+
+export = router;
--- Advanced/Server/src/services/person.service.ts
+++ Advanced/Server/src/services/person.service.ts
@@ -1,4 +1,4 @@
-const getAll = () => {
+export const getAll = () => {
 
 }
 
@@ -20,9 +20,4 @@ const updatePerson = (updatedPerson) => {
 
 const deletePerson = (personToDelete) => {
 
-}
-
-
-module.exports = {
-    getAll,
 }
